9dd34d0949eeb3d5b7563a1e5a92bd15bb1f7b79
scottmcook <scott.madison.cook@gmail.com>
2023-09-23 12:59:11 -0700

    Add containter component for boat search

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class BoatSearch extends LightningElement {
+	handleLoading(){}
+	handleDoneLoading(){}
+	searchBoats(event){}
+	createNewBoat(){}
+}
